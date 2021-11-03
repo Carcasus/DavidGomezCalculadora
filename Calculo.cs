12f20b0109704d14ed51c113d2fafeaea91e3fd4
azhu Gómez Arellano <Azhu@hotmail.es>
2021-11-03 10:30:20 +0100

    Actualizacion, dividido el codigo en dos clases (para convertir en procesos, arreglo de codigo)

---
+++ Calculo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DavidGomezCalculadora
+{
+    class Calculo
+    {
+        internal static double HacerOperacion(double numerador1, double numerador2, string operando)
+        {
+            double resultado = 0;
+            // Use a switch statement to do the math.
+            switch (operando)
+            {
+                case "add":
+                    resultado = numerador1 + numerador2;
+                    break;
+                case "sub":
+                    resultado = numerador1 - numerador2;
+                    break;
+                case "plus":
+                    resultado = numerador1 * numerador2;
+                    break;
+                case "div":
+                    // Ask the user to enter a non-zero divisor.
+                    if (numerador2 != 0)
+                    {
+                        resultado = numerador1 / numerador2;
+                    }
+                    break;
+                // Return text for an incorrect option entry.
+                default:
+                    break;
+            }
+            return resultado;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -17,47 +17,59 @@ namespace DavidGomezCalculadora
             Console.WriteLine("Calculadora por linea de comandos\r");
             Console.WriteLine("------------------------\n");
 
-            Console.WriteLine("Escriba, con un espaciado entre medias, el comando y los dos numeros a usar en la calculadora ");
-            Console.WriteLine("escriba --help en caso de que no sepa como escribir los operandos");
+            Console.WriteLine("Escriba, pulsando enter con cada valor, el comando y los dos numeros a usar en la calculadora ");
+            Console.WriteLine("\tadd");
+            Console.WriteLine("\tsub");
+            Console.WriteLine("\tplus");
+            Console.WriteLine("\tdiv");
+            Console.WriteLine("escriba help en caso de que no sepa como escribir los operandos");
+
+            String num1 = "";
+            String num2 = "";
+            double resultado = 0;
 
             /*Se ha de indicar la operación y dos valores numéricos a tratar.*/
-            var command = args[0];
+            String operando = Console.ReadLine();
+            num1 = Console.ReadLine();
+            num2 = Console.ReadLine();
 
-            double.TryParse(args[1], out double value1);
-            double.TryParse(args[2], out double value2);
+            //Variable para comprobar sin cierre del programa, que las variables numericas son correctas
+            double numerador1 = 0;
+            double numerador2 = 0;
 
             //Dar al usuario la posibilidad de recibir ayuda si no sabe claramente como escribir el codigo
-            if (args.Length == 1 && args[0] == "--help")
+            if (operando == "help")
             {
                 ShowHelp();
             }
-            else if (args.Length != 3)
+            //Linea comprobante del correcto uso de los comandos
+            else if (operando == null || !double.TryParse(num1, out numerador1) || !double.TryParse(num2, out numerador2))
             {
                 ShowError();
                 return;
             }
             else
             {
-                switch (command)
-                {
-                    case "--add":
-                        Add(value1, value2);
-                        break;
-                    case "--sub":
-                        Sub(value1, value2);
-                        break;
-                    case "--plus":
-                        Plus(value1, value2);
-                        break;
-                    case "--div":
-                        Div(value1, value2);
-                        break;
-                    default:
-                        ShowError();
-                        break;
-                }
                 /*A de devolver 0 si el proceso termina correctamente y otro valor si se ha producido un error.*/
-                Console.WriteLine("0");
+                try
+                {
+                    resultado = Calculo.HacerOperacion(numerador1, numerador2, operando);
+                    if (double.IsNaN(resultado))
+                    {
+                        Console.WriteLine("Esta operacion generara un error.\n");
+                    }
+                    else Console.WriteLine("Your result: {0:0.##}\n", resultado);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
+                }
+
+                Console.WriteLine("------------------------\n");
+
+                
+                Console.WriteLine("0\n");
+                Console.WriteLine("Pulsa intro para finalizar el programa");
                 Console.Read(); //Con el read podremos detener momentaneamente la consola
             }
         }
@@ -85,17 +97,19 @@ namespace DavidGomezCalculadora
         private static void Add(double a, double b)
         {
             Console.WriteLine($"{a:N} + {b:N} = {a + b:N}");
-            Console.WriteLine("El valor de sumar " + a + " y " + b + " daria como resultado: "+a + b);
+            Console.WriteLine("El valor de sumar " + a + " y " + b + " daria como resultado: " + (a + b));
         }
 
         private static void Sub(double a, double b)
         {
             Console.WriteLine($"{a:N} - {b:N} = {a - b:N}");
+            Console.WriteLine("El valor de restar " + a + " y " + b + " daria como resultado: " + (a - b));
         }
 
         private static void Plus(double a, double b)
         {
             Console.WriteLine($"{a:N} * {b:N} = {a * b:N}");
+            Console.WriteLine("El valor de multiplicar " + a + " y " + b + " daria como resultado: " + (a * b));
         }
 
         private static void Div(double a, double b)
@@ -103,7 +117,11 @@ namespace DavidGomezCalculadora
             if (b == 0.0)
                 Console.WriteLine("No se dividir entre 0");
             else
+            {
                 Console.WriteLine($"{a:N} / {b:N} = {a / b:N}");
+                Console.WriteLine("El valor de dividir " + a + " y " + b + " daria como resultado: " + (a / b));
+            }
+
         }
     }
 }
